b75e7c1ee123d08080fffe4283da9122d1a758f4
TeaFaris <95927550+TeaFaris@users.noreply.github.com>
2022-06-21 21:33:17 +0500

    фигулина

--- NewContent/Items/Tiles/OfferingAltar.cs
+++ NewContent/Items/Tiles/OfferingAltar.cs
@@ -1,9 +1,41 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ObjectData;
 
 namespace JustEnoughSickles.NewContent.Items.Tiles
 {
     public class OfferingAltar : ModTile
     {
+        public override void SetStaticDefaults()
+        {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileID.Sets.HasOutlines[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
 
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+
+			// Names
+			AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.Altar"));
+
+			// Placement
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+			TileObjectData.newTile.CoordinatePaddingFix = new Point16(0, 2);
+			// The following 3 lines are needed if you decide to add more styles and stack them vertically
+			TileObjectData.newTile.StyleWrapLimit = 2;
+			TileObjectData.newTile.StyleHorizontal = true;
+
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+			TileObjectData.addTile(Type);
+		}
     }
 }
---
+++ NewContent/Items/Tiles/OfferingAltar_Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace JustEnoughSickles.NewContent.Items.Tiles
+{
+	public class OfferingAltarItem : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("This is a... Wait a minute, how did you get that item. Oh no... You are cheating! Quick, delete this item, you are not supposed to get this item!");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.DefaultToPlaceableTile(ModContent.TileType<OfferingAltar>());
+			Item.value = 150;
+			Item.maxStack = 99;
+			Item.width = 12;
+			Item.height = 30;
+		}
+	}
+}
